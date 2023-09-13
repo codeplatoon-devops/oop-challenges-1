ff14dcc3928a339ab5c9fe9ffa325ee0c4c0b820
Stephan Lamoureux <sa.lamoureux@gmail.com>
2023-09-12 20:43:14 -0400

    added new objectives

--- CustomerBankAccount.py
+++ CustomerBankAccount.py
@@ -1,11 +1,28 @@
 """
 1. Create a new class for a CustomerBankAccount.
+
 2. Create fields for the Account Number, Balance, Customer Name, Email and Phone Number.
+
 3. Create methods to read, write and update each field. Basically, some getters and setters.
+
 4. Create two additional methods:
-    i. One to allow the customer to deposit the funds (this should increment the balance field)
-    ii. One to allow the customer to withdraw funds (this should deduct from the balance fields but not allow the withdrawal to complete if their founds are insufficient).
+   i. One to allow the customer to deposit the funds (this should increment the balance field)
+   ii. One to allow the customer to withdraw funds (this should deduct from the balance fields but not allow the withdrawal to complete if their founds are insufficient).
+
 5. Create some validation code using your getters and setters to confirm your code is working at each step of the user flow. You should be able to show that a new user can be created and that this user can deposit and withdraw funds.
+
+6. Now that you've learned about Class Inheritance, let's create some subclasses of the standard BankAccount. First, let's create a StandardCustomerBankAccount with additional fields for tracking Silver Reward Points and tracking Reward Order Transactions. Let's mock the Reward Order Transactions as a list of string objects for now.
+
+7. Create a new class VipCustomer that can earn both Silver and Gold reward points in addition to also having Reward Order Transactions.
+
+8. Create getters and setters for each new field, and implement some validation to confirm your code is working.
+
+9. Let's now create a new Rewards class to represent prizes that customers can exchange reward points for. Fields should include the object name and object price in terms of both Silver and Gold points.
+
+10. We now need to create subclasses of the Rewards Class. We want to be able to distinguish between tangible, physical objects that can be purchased, and digital purchases such as credits towards one's balance.
+    i. DigitalRewards should have a conversion rate. Because Silver points and Gold points have different perceived values, they should reduce a customer's monetary balance at different rates.
+    ii. PhysicalRewards will need an inventory count, because they are not infinitely available like digital rewards are.
+    iii. Create getter, setter functions for all these new fields, and implement validation to confirm your code is working as intended.
 """
 
 
